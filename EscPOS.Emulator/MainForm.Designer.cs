fe595805667c8c79f7397bff35a09b592d9c414a
marianz-bonfire <mariano.degracia.bonfire@gmail.com>
2024-10-05 23:29:45 +0800

    :star: Added necessary project files

---
+++ EscPOS.Emulator/MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace EscPOS.Emulator
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.textBoxMessage = new System.Windows.Forms.TextBox();
+			this.buttonStartServer = new System.Windows.Forms.Button();
+			this.labelStatus = new System.Windows.Forms.Label();
+			this.checkBoxFont = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// textBoxMessage
+			// 
+			this.textBoxMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxMessage.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBoxMessage.Location = new System.Drawing.Point(12, 55);
+			this.textBoxMessage.Multiline = true;
+			this.textBoxMessage.Name = "textBoxMessage";
+			this.textBoxMessage.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBoxMessage.Size = new System.Drawing.Size(453, 518);
+			this.textBoxMessage.TabIndex = 0;
+			// 
+			// buttonStartServer
+			// 
+			this.buttonStartServer.Location = new System.Drawing.Point(390, 12);
+			this.buttonStartServer.Name = "buttonStartServer";
+			this.buttonStartServer.Size = new System.Drawing.Size(75, 23);
+			this.buttonStartServer.TabIndex = 1;
+			this.buttonStartServer.Text = "Start";
+			this.buttonStartServer.UseVisualStyleBackColor = true;
+			this.buttonStartServer.Click += new System.EventHandler(this.buttonStartServer_Click);
+			// 
+			// labelStatus
+			// 
+			this.labelStatus.AutoSize = true;
+			this.labelStatus.Location = new System.Drawing.Point(24, 21);
+			this.labelStatus.Name = "labelStatus";
+			this.labelStatus.Size = new System.Drawing.Size(50, 13);
+			this.labelStatus.TabIndex = 2;
+			this.labelStatus.Text = "Ready...";
+			// 
+			// checkBoxFont
+			// 
+			this.checkBoxFont.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.checkBoxFont.Appearance = System.Windows.Forms.Appearance.Button;
+			this.checkBoxFont.AutoSize = true;
+			this.checkBoxFont.Location = new System.Drawing.Point(12, 578);
+			this.checkBoxFont.Name = "checkBoxFont";
+			this.checkBoxFont.Size = new System.Drawing.Size(52, 23);
+			this.checkBoxFont.TabIndex = 4;
+			this.checkBoxFont.Text = "Courier";
+			this.checkBoxFont.UseVisualStyleBackColor = true;
+			this.checkBoxFont.CheckedChanged += new System.EventHandler(this.checkBoxFont_CheckedChanged);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(477, 605);
+			this.Controls.Add(this.checkBoxFont);
+			this.Controls.Add(this.labelStatus);
+			this.Controls.Add(this.buttonStartServer);
+			this.Controls.Add(this.textBoxMessage);
+			this.Font = new System.Drawing.Font("Tahoma", 8.25F);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "Tarsier Virtual Network POS Printer";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBoxMessage;
+		private System.Windows.Forms.Button buttonStartServer;
+		private System.Windows.Forms.Label labelStatus;
+		private System.Windows.Forms.CheckBox checkBoxFont;
+	}
+}
+
---
+++ EscPOS.Emulator/MainForm.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Reflection.Emit;
+
+namespace EscPOS.Emulator
+{
+	public partial class MainForm : Form
+	{
+		private Thread serverThread = null;
+		private bool running = false;
+		public MainForm() {
+			InitializeComponent();
+		}
+
+		private void StartServer() {
+			// Define the endpoint and create the server socket
+			IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 9100);
+			Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			try {
+				// Bind the socket to the endpoint and start listening for incoming connections
+				serverSocket.Bind(endPoint);
+				serverSocket.Listen(10);
+
+				Console.WriteLine("Server is listening on port 9100...");
+
+				while (running) {
+					// Accept a client connection
+					Socket clientSocket = serverSocket.Accept();
+					Console.WriteLine("Client connected.");
+					AddMessage("", true);
+					// Handle the client connection in a separate thread
+					Thread clientThread = new Thread(HandleClient);
+					clientThread.Start(clientSocket);
+				}
+			} catch (Exception ex) {
+				Console.WriteLine("Error: " + ex.Message);
+			} finally {
+				serverSocket.Close();
+			}
+		}
+
+		void HandleClient(object client) {
+			Socket clientSocket = (Socket)client;
+			byte[] buffer = new byte[4096];
+			int bytesRead;
+
+			try {
+				while ((bytesRead = clientSocket.Receive(buffer)) > 0) {
+					string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+					Console.WriteLine("Received message: " + message);
+					//AddMessage(message, false);
+					String[] chunks = message.Split(new string[] { "\r\n", "\r", "\n" },StringSplitOptions.None);
+					foreach (string chunk in chunks) {
+						AddMessage(chunk, false);
+					}
+					// Echo the message back to the client
+					clientSocket.Send(buffer, bytesRead, SocketFlags.None);
+				}
+			} catch (Exception ex) {
+				Console.WriteLine("Client error: " + ex.Message);
+			} finally {
+				clientSocket.Close();
+			}
+		}
+
+		void AddMessage(string message, bool clear = false) {
+			
+			if (textBoxMessage.InvokeRequired) {
+				if (clear) {
+					textBoxMessage.Invoke(new Action(() => textBoxMessage.Text = ""));
+				} else {
+					textBoxMessage.Invoke(new Action(() => textBoxMessage.Text += (message + Environment.NewLine)));
+				}
+			} else {
+				if (clear) {
+					textBoxMessage.Text = "";
+				} else {
+					textBoxMessage.Text += (message+ Environment.NewLine);
+				}
+			}
+		}
+
+		private void buttonStartServer_Click(object sender, EventArgs e) {
+			if (buttonStartServer.Text.Equals("Start")) {
+				buttonStartServer.Text = "Stop";
+				running = true;
+				AddMessage("", true);
+				labelStatus.Text = "Listening Sirius POS print request...";
+				serverThread = new Thread(new ThreadStart(StartServer));
+				serverThread.Start();
+			} else {
+				running = false;
+				labelStatus.Text = "Server stopped...";
+				buttonStartServer.Text = "Start";
+				AddMessage("", true);
+				if (serverThread != null) {
+					serverThread.Abort();
+					serverThread.Join();
+					serverThread = null;
+				}
+			}
+		}
+
+		private void checkBoxFont_CheckedChanged(object sender, EventArgs e) {
+			textBoxMessage.Font = checkBoxFont.Checked ? new Font("Consolas", 10f) : new Font("Courier New", 10f);
+			checkBoxFont.Text = checkBoxFont.Checked ? "Courier" : "Consolas";
+		}
+	}
+}
